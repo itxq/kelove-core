da0bfc009d514cb14e3d7d7ce9ea6159ff10a844
IT小强xqitw.cn <360237521@qq.com>
2019-01-19 16:33:56 +0800

    run 类 更新

--- kelove/core/Run.php
+++ kelove/core/Run.php
@@ -15,6 +15,7 @@ namespace kelove\core;
 use Composer\Autoload\ClassLoader;
 use kelove\traits\SingleModelTrait;
 use think\Exception;
+use think\facade\Event;
 
 /**
  * 初始化应用
@@ -26,41 +27,82 @@ class Run
     use SingleModelTrait;
     
     /**
+     * 应用列表事件标识
+     */
+    const APP_LIST_EVENT = 'kelove_app_list';
+    
+    /**
+     * @var string 应用入口文件
+     */
+    protected $scriptName;
+    
+    /**
      * @var string 核心目录
      */
-    protected $kelovePath = '';
+    protected $kelovePath;
+    
+    /**
+     * @var array 应用列表
+     */
+    protected $appList = [];
+    
+    /**
+     * @var \kelove\core\App
+     */
+    protected $app;
     
     /**
      * 初始化加载
      * @param array $config - 配置信息
      */
     protected function initialize(array $config = []): void {
+        $this->scriptName = $this->getScriptName();
         $this->kelovePath = realpath(__DIR__ . '/../') . DIRECTORY_SEPARATOR;
+        $this->appList = $this->getAppList();
+        $this->app = new App($this->kelovePath);
+    }
+    
+    /**
+     * 获取全部应用列表
+     * @return mixed
+     */
+    protected function getAppList() {
+        $list = [];
+        $appList = Event::trigger(self::APP_LIST_EVENT);
+        foreach ($appList as $v) {
+            if (!$this->appInfoCheck($v)) {
+                continue;
+            }
+            $list[$v['app_name']] = $v;
+        }
+        return $list;
     }
     
     /**
      * 初始化应用
-     * @param \Composer\Autoload\ClassLoader $loader
-     * @param bool $debug 是否开启调试
      * @param string $name 应用名称
+     * @param bool $debug 是否开启调试
      * @param string $namespace 应用命名空间
      * @param string $path 应用路径
      */
-    public function appRun(ClassLoader $loader, bool $debug = false, string $name = '', string $namespace = '', string $path = ''): void {
+    public function appRun(string $name = '', bool $debug = false): void {
         ini_set('display_errors', 'Off');
-        if (empty($path)) {
-            $path = $this->getNamespacePath($loader, $namespace);
+        if (empty($name)) {
+            $name = pathinfo($this->scriptName, PATHINFO_FILENAME);
         }
         try {
-            $app = new App($this->kelovePath);
-            $app->debug($debug)
+            $appInfo = $this->appList[$name];
+            $path = $appInfo['app_path'];
+            $namespace = $appInfo['app_namespace'];
+            $this->app->debug($debug)
                 ->setBasePath($this->kelovePath)
                 ->setRootRuntimePath('runtime' . DIRECTORY_SEPARATOR)
                 ->name($name)
                 ->setNamespace($namespace)
                 ->path($path)
                 ->run()->send();
-        } catch (Exception $exception) {
+        } catch
+        (Exception $exception) {
             exit();
         }
     }
@@ -81,5 +123,30 @@ class Run
             return '';
         }
         return $path . DIRECTORY_SEPARATOR;
+    }
+    
+    /**
+     * 检查信息配置信息
+     * @param array $info
+     * @return bool
+     */
+    protected function appInfoCheck(array $info): bool {
+        if (
+            !isset($info['app_name']) ||
+            !isset($info['app_title']) ||
+            !isset($info['app_path']) ||
+            !isset($info['app_namespace'])
+        ) {
+            return false;
+        }
+        return true;
+    }
+    
+    /**
+     * 获取入口文件
+     * @return string
+     */
+    protected function getScriptName(): string {
+        return 'cli' == PHP_SAPI ? realpath($_SERVER['argv'][0]) : $_SERVER['SCRIPT_FILENAME'];
     }
 }
